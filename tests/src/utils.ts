2001395ce4bf45b06ad5fe114f4322b90c08a390
Wesley Finck <wesleyfinck@gmail.com>
2023-01-19 13:06:42 -0600

    bump to beta-rc.2 happ tests passing

--- tests/src/utils.ts
+++ tests/src/utils.ts
@@ -4,6 +4,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const todoListsDna = path.join(__dirname, "../../dna/workdir/todo_lists.dna");
+export const todoListsHapp = path.join(__dirname, "../../workdir/todo.happ");
 
 
 
