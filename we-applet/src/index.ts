ab38484bb8de839f70d9b8b4c3d3e4cf5eaef2ae
Wesley Finck <wesleyfinck@gmail.com>
2023-01-27 12:40:20 -0600

    cleanup logs and format files

--- we-applet/src/index.ts
+++ we-applet/src/index.ts
@@ -19,27 +19,12 @@ const todoApplet: WeApplet = {
   ): Promise<AppletRenderers> {
     return {
       full(element: HTMLElement, registry: CustomElementRegistry) {
-        console.log("in the applet in we")
-        console.log("registry", registry);
-        console.log("applet app info", appletAppInfo)
-        try {
-
-          registry.define("todo-applet", TodoApplet);
-          // element.innerHTML = `<script src="/node_modules/@webcomponents/scoped-custom-element-registry/scoped-custom-element-registry.min.js"></script>`
-          element.innerHTML = `<todo-applet></todo-applet>`;
-        }
-        catch (e) {
-          console.log('error in registry', e)
-          throw e
-        }
-        // registry.upgrade("todo-applet", TodoApplet);
+        registry.define("todo-applet", TodoApplet);
+        element.innerHTML = `<todo-applet></todo-applet>`;
         const appletElement = element.querySelector("todo-applet") as any;
         appletElement.appWebsocket = appWebsocket;
-        appletElement.appWebsocket = adminWebsocket;
         appletElement.appletAppInfo = appletAppInfo;
         appletElement.sensemakerStore = weStore.sensemakerStore;
-        console.log('sensemaker store in index', weStore.sensemakerStore)
-        console.log('profile store in index', weStore.profilesStore)
       },
       blocks: [],
     };
