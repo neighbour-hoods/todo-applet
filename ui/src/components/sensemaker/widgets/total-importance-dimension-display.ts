5501941fdb4897f63b42ecba1a32e6196899d625
Wesley Finck <wesleyfinck@gmail.com>
2023-06-17 12:55:20 -0600

    basic rendering of total importance widget

--- ui/src/components/sensemaker/widgets/total-importance-dimension-display.ts
+++ ui/src/components/sensemaker/widgets/total-importance-dimension-display.ts
@@ -2,6 +2,7 @@ import { css, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { DisplayDimensionWidget } from '@neighbourhoods/client';
 import { Assessment, RangeValueInteger } from '@neighbourhoods/client';
+import { variables } from '../../../styles/variables';
 
 @customElement('total-importance-dimension-display')
 export class TotalImportanceDimensionDisplay extends DisplayDimensionWidget {
@@ -11,8 +12,10 @@ export class TotalImportanceDimensionDisplay extends DisplayDimensionWidget {
 
     render() {
         return html`
-                    <div>
-                        (${this.assessment ? (this.assessment.value as RangeValueInteger).Integer : 0})
+                    <div class="display-box-wrapper">            
+                        <div class="display-box">
+                            (${this.assessment ? (this.assessment.value as RangeValueInteger).Integer : 0})
+                        </div>
                     </div>
                 `
     }
@@ -20,11 +23,39 @@ export class TotalImportanceDimensionDisplay extends DisplayDimensionWidget {
         return {
         }
     }
-    static styles = css`
-        .heat-scale {
-            display: flex;
-            flex-direction: row;
-        }
-    `
+    static get styles() {
+        return [
+            variables,
+            css`
+            .heat-scale {
+                display: flex;
+                flex-direction: row;
+            }
+            .display-box {
+                background-color: var(--nh-theme-accent-muted);
+                padding: 2px;
+                border-radius: 50%;
+                width: 32px;
+                height: 32px;
+                display: flex;
+                margin: 2px;
+                border-color: var(--nh-theme-accent-muted);
+                border-style: solid;
+                justify-content: center;
+                align-items: center;
+                position: relative;
+                top: 50%;
+                transform: translateY(-55%);
+                font-size: 16px;
+                color: var(--nh-theme-fg-default);
+            }
+            .display-box-wrapper {
+                position: relative;
+                align-items: center;
+                justify-content: center;
+            }
+            `
+            ]
+    }
 }
 
