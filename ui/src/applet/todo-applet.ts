bc6c1f2c28694a6db2ee344202aa97af20befe36
Wesley Finck <wesleyfinck@gmail.com>
2023-07-10 15:41:31 -0700

    WIP: render applet in the same way it is in nh-launcher but for dev mode

---
+++ ui/src/applet/todo-applet.ts
@@ -0,0 +1,104 @@
+import { property, state } from "lit/decorators.js";
+import { ScopedElementsMixin } from "@open-wc/scoped-elements";
+import { CircularProgress } from "@scoped-elements/material-web";
+import { LitElement, html, css } from "lit";
+import { AppletInfo } from "@neighbourhoods/nh-launcher-applet";
+import { TodoApp, TodoStore, appletConfig, ImportanceDimensionAssessment, TotalImportanceDimensionDisplay, HeatDimensionAssessment, AverageHeatDimensionDisplay } from "../index";
+import { AppWebsocket, CellType, ProvisionedCell, encodeHashToBase64 } from "@holochain/client";
+import { SensemakerStore } from "@neighbourhoods/client";
+import { get } from 'svelte/store';
+
+export class TodoApplet extends ScopedElementsMixin(LitElement) {
+  @property()
+  appletAppInfo!: AppletInfo[];
+
+  @property()
+  appWebsocket!: AppWebsocket;
+
+  @property()
+  sensemakerStore!: SensemakerStore;
+
+  @property()
+  todoStore!: TodoStore;
+
+  @state()
+  loaded = false;
+
+  async firstUpdated() {
+    try {
+      const appletRoleName = "todo_lists";
+      const todoAppletInfo = this.appletAppInfo[0];
+      const cellInfo = todoAppletInfo.appInfo.cell_info[appletRoleName][0]
+      const todoCellInfo = (cellInfo as { [CellType.Provisioned]: ProvisionedCell }).provisioned;
+
+      await this.sensemakerStore.registerApplet(appletConfig)
+
+      await this.sensemakerStore.registerWidget(
+        [
+          encodeHashToBase64(get(this.sensemakerStore.appletConfig()).dimensions["importance"]),
+          encodeHashToBase64(get(this.sensemakerStore.appletConfig()).dimensions["total_importance"]),
+        ],
+        TotalImportanceDimensionDisplay,
+        ImportanceDimensionAssessment
+      )
+      await this.sensemakerStore.registerWidget(
+        [
+          encodeHashToBase64(get(this.sensemakerStore.appletConfig()).dimensions["perceived_heat"]),
+          encodeHashToBase64(get(this.sensemakerStore.appletConfig()).dimensions["average_heat"]),
+        ],
+        AverageHeatDimensionDisplay,
+        HeatDimensionAssessment
+      )
+      const appWs = await AppWebsocket.connect(this.appWebsocket.client.socket.url)
+      this.todoStore = new TodoStore(
+        appWs,
+        todoCellInfo.cell_id,
+        appletRoleName
+      );
+      const allTasks = await this.todoStore.fetchAllTasks()
+      const allTaskEntryHashes = get(this.todoStore.allTaskEntryHashes())
+      const importanceDimensionEh = get(this.sensemakerStore.appletConfig()).dimensions["importance"]
+      const totalImportanceDimensionEh = get(this.sensemakerStore.appletConfig()).dimensions["total_importance"]
+      const perceivedHeatDimensionEh = get(this.sensemakerStore.appletConfig()).dimensions["perceived_heat"]
+      const averageHeatDimensionEh = get(this.sensemakerStore.appletConfig()).dimensions["average_heat"]
+      await this.sensemakerStore.getAssessmentsForResources({
+      dimension_ehs: null,
+      resource_ehs: allTaskEntryHashes
+    })
+      this.loaded = true;
+    }
+    catch (e) {
+      console.log("error in first update", e)
+    }
+  }
+  static styles = css`
+    .completed {
+      text-decoration-line: line-through;
+      color: #777;
+    }
+  `;
+
+  render() {
+    if (!this.loaded)
+      return html`<div
+        style="display: flex; flex: 1; flex-direction: row; align-items: center; justify-content: center"
+      >
+        <mwc-circular-progress indeterminate></mwc-circular-progress>
+      </div>`;
+    return html`
+      <todo-app style="height: 100vh; width: 100%; margin-bottom: 70px" .sensemakerStore=${this.sensemakerStore} .todoStore=${this.todoStore}></todo-app>
+    `;
+  }
+
+  static get scopedElements() {
+    return {
+      "mwc-circular-progress": CircularProgress,
+      "todo-app": TodoApp,
+      'total-importance-dimension-display': TotalImportanceDimensionDisplay,
+      'importance-dimension-assessment': ImportanceDimensionAssessment,
+      'average-heat-dimension-display': AverageHeatDimensionDisplay,
+      'heat-dimension-assessment': HeatDimensionAssessment,
+      // TODO: add any elements that you have in your applet
+    };
+  }
+}
---
+++ ui/src/bundle-harness.ts
@@ -0,0 +1,201 @@
+import { LitElement, css, html } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
+import {
+  AppWebsocket,
+  ActionHash,
+  AppInfo,
+  AdminWebsocket,
+  encodeHashToBase64,
+  CellInfo,
+  AppAgentWebsocket,
+  ProvisionedCell,
+  CellType,
+  CellId,
+  ClonedCell,
+} from '@holochain/client';
+import '@material/mwc-circular-progress';
+import { ScopedElementsMixin } from '@open-wc/scoped-elements';
+import { TodoStore } from './todo-store';
+import { CreateOrJoinNh } from './create-or-join-nh';
+import { SensemakerStore } from '@neighbourhoods/client';
+import { appletConfig } from './appletConfig'
+import todoApplet from './applet/index'
+import { AppletInfo, AppletRenderers } from '@neighbourhoods/nh-launcher-applet';
+import { RenderBlock } from "./render-block";
+
+@customElement('bundle-harness')
+export class BundleHarness extends ScopedElementsMixin(LitElement) {
+  @state() loading = true;
+  @state() actionHash: ActionHash | undefined;
+  @state() currentSelectedList: string | undefined;
+
+  @property({ type: Object })
+  appWebsocket!: AppWebsocket;
+
+  @property({ type: Object })
+  adminWebsocket!: AdminWebsocket;
+
+  @property({ type: Object })
+  appInfo!: AppInfo;
+
+  @property()
+  _todoStore!: TodoStore;
+
+  @property()
+  _sensemakerStore!: SensemakerStore;
+
+  @property()
+  isSensemakerCloned: boolean = false;
+
+  @property()
+  agentPubkey!: string;
+
+  renderers!: AppletRenderers;
+  appletInfo!: AppletInfo[];
+
+
+  async firstUpdated() {
+    // connect to holochain conductor and set up websocket connections
+    try {
+      await this.connectHolochain()
+      const installedCells = this.appInfo.cell_info;
+
+      // check if sensemaker has been cloned yet
+      const sensemakerCellInfo: CellInfo[] = installedCells["sensemaker"];
+      const todoCellInfo: CellInfo[] = installedCells["todo_lists"];
+      let todoCellId: CellId;
+      if (CellType.Provisioned in todoCellInfo[0]) {
+        todoCellId = (todoCellInfo[0][CellType.Provisioned] as ProvisionedCell).cell_id;
+      } else {
+        throw new Error("todo_lists cell not provisioned yet")
+
+      }
+      this.agentPubkey = encodeHashToBase64(todoCellId[1]);
+
+      // mocking what gets passed to the applet
+      this.appletInfo = [{
+        weInfo: {
+            logoSrc: "",
+            name: ""
+        },
+        appInfo: this.appInfo
+      }];
+    }
+    catch (e) {
+      console.log("error registering applet", e)
+    }
+  }
+
+  async initializeSensemakerStore(clonedSensemakerRoleName: string) {
+    const appAgentWebsocket: AppAgentWebsocket = await AppAgentWebsocket.connect(``, "todo-sensemaker");
+    this._sensemakerStore = new SensemakerStore(appAgentWebsocket, clonedSensemakerRoleName);
+    // @ts-ignore
+    this.renderers = await todoApplet.appletRenderers(this.appWebsocket, this.adminWebsocket, { sensemakerStore: this._sensemakerStore }, this.appletInfo);
+  }
+  async cloneSensemakerCell(ca_pubkey: string) {
+    const clonedSensemakerCell: ClonedCell = await this.appWebsocket.createCloneCell({
+      app_id: 'todo-sensemaker',
+      role_name: "sensemaker",
+      modifiers: {
+        network_seed: '',
+        properties: {
+          sensemaker_config: {
+            neighbourhood: "todo test",
+            wizard_version: "v0.1",
+            community_activator: ca_pubkey
+          },
+          applet_configs: [],
+        },
+    }});
+    this.isSensemakerCloned = true;
+    await this.initializeSensemakerStore(clonedSensemakerCell.clone_id)
+  }
+
+  async createNeighbourhood(_e: CustomEvent) {
+    await this.cloneSensemakerCell(this.agentPubkey)
+    const _todoConfig = await this._sensemakerStore.registerApplet(appletConfig);
+    
+    this.loading = false;
+  }
+
+  async joinNeighbourhood(e: CustomEvent) {
+    await this.cloneSensemakerCell(e.detail.newValue)
+    // wait some time for the dht to sync, otherwise checkIfAppletConfigExists returns null
+    setTimeout(async () => {
+      const _todoConfig = await this._sensemakerStore.registerApplet(appletConfig);
+      this.loading = false;
+    }, 2000)
+  }
+
+  render() {
+    if (this.isSensemakerCloned && this.loading)
+      return html`
+        <mwc-circular-progress indeterminate></mwc-circular-progress>
+      `;
+    if (!this.isSensemakerCloned)
+      return html`
+        <create-or-join-nh @create-nh=${this.createNeighbourhood} @join-nh=${this.joinNeighbourhood}></create-or-join-nh>
+      `;
+    return html`
+      <main>
+        <h3>My Pubkey: ${this.agentPubkey}</h3>
+        <div class="home-page">
+        <render-block
+            .renderer=${this.renderers.full}
+            style="flex: 1"
+        ></render-block>
+        </div>
+      </main>
+    `;
+  }
+
+  async connectHolochain() {
+    this.adminWebsocket = await AdminWebsocket.connect(``);
+    this.appWebsocket = await AppWebsocket.connect(``);
+    this.appInfo = await this.appWebsocket.appInfo({
+      installed_app_id: 'todo',
+    });
+  }
+
+  static get scopedElements() {
+    return {
+      'create-or-join-nh': CreateOrJoinNh,
+      'render-block': RenderBlock,
+    };
+  }
+
+  static styles = css`
+    .home-page {
+      display: flex;
+      flex-direction: row;
+    }  
+
+    :host {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
+      font-size: calc(10px + 2vmin);
+      color: #1a2b42;
+      max-width: 960px;
+      margin: 0 auto;
+      text-align: center;
+      background-color: var(--lit-element-background-color);
+    }
+
+    main {
+      flex-grow: 1;
+    }
+
+    .app-footer {
+      font-size: calc(12px + 0.5vmin);
+      align-items: center;
+    }
+
+    .app-footer a {
+      margin-left: 5px;
+    }
+  `;
+}
+
