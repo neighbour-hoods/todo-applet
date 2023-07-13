22933c39608f9a9fb08074b6e6f9f53b327f8497
Wesley Finck <wesleyfinck@gmail.com>
2023-07-13 11:57:43 -0700

    use vite for dev build and rollup for production build

---
+++ ui/dev.vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  test: {
+    /* for example, use global to avoid globals imports (describe, test, expect): */
+    globals: true,
+    deps: {
+        inline: [/@neighbourhoods/, /@scoped-elements\/shoelace/]
+      }
+  },
+});
