6296549b24a7ab3043f3883e85d74c3a13cd92b2
Nicholas Stebbings <nickolarse@riseup.net>
2024-02-23 11:50:32 +1300

    fix styles, tidy up harness

--- ui/src/applet-test-harness.ts
+++ ui/src/applet-test-harness.ts
@@ -1,67 +1,31 @@
 import { LitElement, css, html } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
-import {
-  AppWebsocket,
-  ActionHash,
-  AppInfo,
-  AdminWebsocket,
-  encodeHashToBase64,
-  CellInfo,
-  AppAgentWebsocket,
-  ProvisionedCell,
-  CellType,
-  CellId,
-  ClonedCell,
-} from '@holochain/client';
 import { ScopedRegistryHost } from '@lit-labs/scoped-registry-mixin';
-import { TodoStore } from './todo-store';
+
+import { AppWebsocket, AppInfo, AdminWebsocket, encodeHashToBase64, CellInfo, AppAgentWebsocket, ProvisionedCell, CellType, CellId, ClonedCell, } from '@holochain/client';
+
+import { connectHolochainApp, getAppAgentWebsocket, createAppDelegate, AppBlockRenderer } from "@neighbourhoods/app-loader"
 import { CreateOrJoinNH } from '@neighbourhoods/dev-util-components';
-import {
-  SensemakerStore,
-} from '@neighbourhoods/client';
+import { SensemakerStore, } from '@neighbourhoods/client';
+
+import { CircularProgress } from '@scoped-elements/material-web';
+
+import { TodoStore } from './todo-store';
 import { INSTALLED_APP_ID, appletConfig } from './appletConfig'
 import TodoApplet from './applet-index'
-import './components/task-display-wrapper'
-import {
-  connectHolochainApp,
-  getAppAgentWebsocket,
-  createAppDelegate,
-  AppBlockRenderer
-} from "@neighbourhoods/app-loader"
-import { CircularProgress } from '@scoped-elements/material-web';
 
 @customElement('applet-test-harness')
 export class AppletTestHarness extends ScopedRegistryHost(LitElement) {
   @state() loading = true;
-  @state() actionHash: ActionHash | undefined;
-  @state() currentSelectedList: string | undefined;
-
-  @property({ type: Object })
-  appWebsocket!: AppWebsocket;
-
-  @property({ type: Object })
-  adminWebsocket!: AdminWebsocket;
-
-  @property()
-  appInfo!: AppInfo;
-
-  @property()
-  _todoStore!: TodoStore;
-
-  @property()
-  _sensemakerStore!: SensemakerStore;
-
-  @property()
-  isSensemakerCloned: boolean = false;
-
-  @property()
-  agentPubkey!: string;
-
-  @state()
-  taskHash?: ActionHash;
-
-  appAgentWebsocket!: AppAgentWebsocket;
-
+  @state() isSensemakerCloned: boolean = false;
+  
+  private _agentPubkey!: string;
+  private _appWebsocket!: AppWebsocket;
+  private _adminWebsocket!: AdminWebsocket;
+  private _appAgentWebsocket!: AppAgentWebsocket;
+  
+  private _sensemakerStore!: SensemakerStore;
+  private _appInfo!: AppInfo;
 
   async firstUpdated() {
     // connect to holochain conductor and set up websocket connections
@@ -73,9 +37,9 @@ export class AppletTestHarness extends ScopedRegistryHost(LitElement) {
       } = await connectHolochainApp(INSTALLED_APP_ID);
       const installedCells = appInfo.cell_info
 
-      this.adminWebsocket = adminWebsocket
-      this.appWebsocket = appWebsocket
-      this.appInfo = appInfo
+      this._adminWebsocket = adminWebsocket
+      this._appWebsocket = appWebsocket
+      this._appInfo = appInfo
       // check if sensemaker has been cloned yet
       const sensemakerCellInfo: CellInfo[] = installedCells["sensemaker"];
       if (sensemakerCellInfo.length > 1) {
@@ -92,7 +56,7 @@ export class AppletTestHarness extends ScopedRegistryHost(LitElement) {
         throw new Error("todo_lists cell not provisioned yet")
 
       }
-      this.agentPubkey = encodeHashToBase64(todoCellId[1]);
+      this._agentPubkey = encodeHashToBase64(todoCellId[1]);
     }
     catch (e) {
       console.log("error registering applet", e)
@@ -100,13 +64,12 @@ export class AppletTestHarness extends ScopedRegistryHost(LitElement) {
   }
 
   async initializeSensemakerStore(clonedSensemakerRoleName: string) {
-    const appAgentWebsocket = await getAppAgentWebsocket(INSTALLED_APP_ID);
-    this.appAgentWebsocket = appAgentWebsocket;
-    this._sensemakerStore = new SensemakerStore(appAgentWebsocket, clonedSensemakerRoleName);
-    await this._sensemakerStore.registerApplet(TodoApplet.appletConfig)
+    this._appAgentWebsocket = await getAppAgentWebsocket(INSTALLED_APP_ID);;
+    this._sensemakerStore = new SensemakerStore(this._appAgentWebsocket, clonedSensemakerRoleName);
   }
+
   async cloneSensemakerCell(ca_pubkey: string) {
-    const clonedSensemakerCell: ClonedCell = await this.appWebsocket.createCloneCell({
+    const clonedSensemakerCell: ClonedCell = await this._appWebsocket.createCloneCell({
       app_id: INSTALLED_APP_ID,
       role_name: "sensemaker",
       modifiers: {
@@ -121,14 +84,14 @@ export class AppletTestHarness extends ScopedRegistryHost(LitElement) {
         },
     }});
     this.isSensemakerCloned = true;
-    await this.adminWebsocket.authorizeSigningCredentials(clonedSensemakerCell.cell_id);
+    await this._adminWebsocket.authorizeSigningCredentials(clonedSensemakerCell.cell_id);
     await this.initializeSensemakerStore(clonedSensemakerCell.clone_id)
   }
 
   async createNeighbourhood(_e: CustomEvent) {
-    await this.cloneSensemakerCell(this.agentPubkey)
+    await this.cloneSensemakerCell(this._agentPubkey)
     const _todoConfig = await this._sensemakerStore.registerApplet(appletConfig);
-console.log('_todoConfig :>> ', _todoConfig);
+    console.log('App config :>> ', _todoConfig);
     this.loading = false;
   }
 
@@ -136,6 +99,7 @@ console.log('_todoConfig :>> ', _todoConfig);
     await this.cloneSensemakerCell(e.detail.newValue)
     // wait some time for the dht to sync, otherwise checkIfAppletConfigExists returns null
     setTimeout(async () => {
+      console.log('attempt to install/join from agent 2:>> ');
       const _todoConfig = await this._sensemakerStore.registerApplet(appletConfig);
       this.loading = false;
     }, 2000)
@@ -151,8 +115,8 @@ console.log('_todoConfig :>> ', _todoConfig);
         <create-or-join-nh @create-nh=${this.createNeighbourhood} @join-nh=${this.joinNeighbourhood}></create-or-join-nh>
       `;
     const delegate = createAppDelegate(
-      this.appAgentWebsocket,
-      this.appInfo,
+      this._appAgentWebsocket,
+      this._appInfo,
       {
         logoSrc: "",
         name: ""
@@ -161,11 +125,11 @@ console.log('_todoConfig :>> ', _todoConfig);
     )
     return html`
       <main>
-        <h3>My Pubkey: ${this.agentPubkey}</h3>
-        <div class="home-page"
-          @task-hash-created=${(e: CustomEvent) => { console.log('task created with hash:', e.detail.hash); this.taskHash = e.detail.hash }}
+        <h3>My Pubkey: ${this._agentPubkey}</h3>
+        <div
+          @task-hash-created=${(e: CustomEvent) => { console.log('task created with hash:', e.detail.hash) }}
         >
-        <app-renderer .component=${TodoApplet.appletRenderers['full']} .nhDelegate=${delegate}></app-renderer>
+          <app-renderer .component=${TodoApplet.appletRenderers['full']} .nhDelegate=${delegate}></app-renderer>
         </div>
       </main>
     `;
@@ -180,36 +144,34 @@ console.log('_todoConfig :>> ', _todoConfig);
   }
 
   static styles = css`
-    .home-page {
-      display: flex;
-      flex-direction: row;
+    :host {
+      width: 100%;
+      height: 100%;
     }
 
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--lit-element-background-color);
+    app-renderer {
+      width: 100%;
+    }
+
+    create-or-join-nh {
+      width: 50vw;
+      display: block;
+      margin: 30vh auto;
+      height: 50vh;
     }
 
     main {
       flex-grow: 1;
     }
 
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
+    h3 {
+      text-align: center;
     }
 
-    .app-footer a {
-      margin-left: 5px;
+    mwc-circular-progress {
+      position: fixed;
+      top: 45vh;
+      right: 48vw;
     }
   `;
 }
