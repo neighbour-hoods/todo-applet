9f961adc2fc72f833bf75c1001f557e386a7b535
Nicholas Stebbings <nickolarse@riseup.net>
2024-07-09 12:15:29 +0100

    update old structure so it will still package once using composition

--- ui/old-applet-structure/vite.config.js
+++ ui/old-applet-structure/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig, loadEnv } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig(({ mode }) => {
   const isProduction = mode === 'production'
 
   return isProduction ? {
-    plugins: [
+    plugins: [ svelte(),
       {
         name: 'copy-assets',
         apply: 'build',
