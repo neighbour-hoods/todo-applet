d2e42d6a1cf8baac1745cb4bd366619503c4a8e1
Nicholas Stebbings <nickolarse@riseup.net>
2024-07-31 15:12:19 +0100

    update flake/hdk/hdi to holochain 0.3.1

--- flake.nix
+++ flake.nix
@@ -1,36 +1,34 @@
 {
-  description = "Template for Holochain app development";
+  description = "Holochain Development Env";
 
   inputs = {
-    versions.url  = "github:holochain/holochain?dir=versions/0_2";
-
-    holochain-flake.url = "github:holochain/holochain";
-    holochain-flake.inputs.versions.follows = "versions";
-    holochain-flake.inputs.holochain.url = "github:holochain/holochain/holochain-0.2.2";
-
     nixpkgs.follows = "holochain-flake/nixpkgs";
     flake-parts.follows = "holochain-flake/flake-parts";
+    holochain-nix-versions.url  = "github:holochain/holochain/?dir=versions/0_2";
+
+    holochain-flake = {
+      url = "github:holochain/holochain";
+      inputs.holochain.url = "github:holochain/holochain/holochain-0.3.1";
+      inputs.lair.url = "github:holochain/lair/lair_keystore-v0.4.4";
+    };
   };
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake
+  outputs = inputs @ { ... }:
+    inputs.holochain-flake.inputs.flake-parts.lib.mkFlake
       {
         inherit inputs;
       }
       {
         systems = builtins.attrNames inputs.holochain-flake.devShells;
         perSystem =
-          { inputs'
-          , config
+          { config
           , pkgs
           , system
           , ...
           }: {
             devShells.default = pkgs.mkShell {
-              inputsFrom = [ inputs'.holochain-flake.devShells.holonix ];
+              inputsFrom = [ inputs.holochain-flake.devShells.${system}.holonix ];
               packages = with pkgs; [
-                pkgs.nodejs-18_x
-                nodePackages.pnpm
               ];
             };
           };
