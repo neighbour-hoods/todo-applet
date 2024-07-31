21cfb55b8ecd23658693845c7404d4646a6a8beb
Nicholas Stebbings <nickolarse@riseup.net>
2024-07-31 11:02:00 +0100

    update Task resource view story

--- ui/views/Task/ResourceView/src/task-resource-view.ts
+++ ui/views/Task/ResourceView/src/task-resource-view.ts
@@ -1,22 +1,30 @@
 import { property, state } from "lit/decorators.js";
-import { Task, WrappedEntry } from "./types";
-import { NHComponent } from "@neighbourhoods/design-system-components";
-import { CheckListItem, Checkbox, ListItem } from "@scoped-elements/material-web";
+import { Task } from "./types";
+import { NHCheckbox, NHComponent } from "@neighbourhoods/design-system-components";
+import { CSSResult, css, html } from "lit";
 
 export class TaskResourceView extends NHComponent {
   @property() @state()
-  task!: WrappedEntry<Task>
+  task!: Task
 
   render() {
-    return `
+    return html`
       <div class="task-item-container">
-        <mwc-check-list-item class="check-list-item"
-          left 
-          ?selected=${Object.keys(this.task.entry.status)[0] == "Complete"} 
-          @click=${this.dispatchTaskToggle}
-        >
-          ${this.task.entry.description}
-        </mwc-check-list-item>
+        <slot name="vote"></slot>
+          <div class="task-details">
+            <nh-checkbox
+              .size=${"auto"}
+              .label=${""}
+              name=${"task-item"}
+              class="check-list-item"
+              @click=${this.dispatchTaskToggle}
+            >
+            </nh-checkbox>
+
+            <label for="task-item">${this.task.description}</label>
+          </div>
+
+        <slot name="input-assessment"></slot>
       </div>
     `
   }
@@ -26,9 +34,55 @@ export class TaskResourceView extends NHComponent {
   }
 
   static elementDefinitions = {
-    'mwc-checkbox': Checkbox,
-    'mwc-list-item': ListItem,
-    'mwc-check-list-item': CheckListItem,
+    'nh-checkbox': NHCheckbox,
   }
+
+  static styles: CSSResult[] = [
+    super.styles as CSSResult,
+    css`
+        :host {
+            display: flex;
+            height: 2rem;
+        }
+
+        .task-item-container {
+            display: flex;
+            flex: 1;
+            font-size: 16px;
+            color: #fff;
+            background-color: var(--nh-theme-bg-surface);
+            border-radius: 8px;
+            margin: 0;
+            align-items: center;
+            height: 100%;
+            padding: 8px;
+            box-sizing: border-box;
+            justify-content: flex-start;
+            display: flex;
+            line-height: 2rem;
+        }
+
+        .task-details {
+            display: flex;
+            flex: 1;
+            flex-basis: 50%;
+            background-color: var(--nh-theme-bg-canvas);
+            align-items: center;
+            border-radius: 8px;
+            margin: 0px 8px 0 4px;
+        }
+        
+        .check-list-item, nh-checkbox {
+            display: flex;
+            flex: 1 1 0%;
+            flex: initial;
+        }
+            
+        nh-checkbox {
+            width: 2rem;
+            margin-right: 24px;
+        }
+    `
+  ]
 }
 
---
+++ ui/views/Task/ResourceView/task-resource-view.stories.ts
@@ -0,0 +1,39 @@
+import { html } from 'lit';
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { TaskResourceView } from './dist/task-resource-view';
+import { WrappedEntry, Task } from './dist/types';
+
+customElements.define('task-resource-view', TaskResourceView)
+
+interface TaskResourceViewProps {
+  task: Task;
+}
+
+const meta: Meta<TaskResourceViewProps> = {
+  title: 'TodoApplet/Task/ResourceView',
+  component: 'task-resource-view',
+  argTypes: {
+  },
+  parameters: { 
+    backgrounds: { default: 'surface' },
+  },
+  render: (args) => html`<task-resource-view
+    .task=${args.task}
+  ></task-resource-view>`,
+};
+
+export default meta;
+
+type Story = StoryObj<TaskResourceViewProps>;
+
+export const Default: Story = {
+  args: {
+    task: {
+      description: "Do a thing",
+        status: {
+          Complete: null,
+      },
+      list: "List 1"
+    }
+  },
+};
